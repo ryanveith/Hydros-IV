c5b42c4cb9c54aa79cba0b42e5b26ef5ed0046d2
ryanveith <ryanleeveith@gmail.com>
2026-07-14 18:14:47 -0700

    ready to merge

--- terrain/square.py
+++ terrain/square.py
@@ -9,7 +9,7 @@ class Square(drawable_object.Drawable_Object):
         self.world = "Hydros"        
         self.terrain = "ground"
         self.occupied = None
-        self.resources = [];
+        self.resources = []
 
     def draw_self(self, zoom: int, screen_x: int, screen_y: int, canvas: Canvas, mode: str, tkinter_image_list: dict[str, ImageTk.PhotoImage]):
         super().draw_self(zoom, screen_x, screen_y, canvas, mode, tkinter_image_list)
