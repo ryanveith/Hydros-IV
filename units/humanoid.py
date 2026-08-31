fb621b2653dc309bbc5392af6132ab85b09e8b4d
ryanveith <ryanleeveith@gmail.com>
2026-08-30 21:45:08 -0400

    hair is still horrid but at least units are something if not what I first envisioned

--- units/humanoid.py
+++ units/humanoid.py
@@ -4,9 +4,8 @@ from units.unit import Unit
 
 
 class Humanoid(Unit):
-    SPRITE_SIZE = 100
     HAIR_X_OFFSET = 0
-    HAIR_Y_OFFSET = -5
+    HAIR_Y_OFFSET = -20
 
     def __init__(
         self,
@@ -23,19 +22,20 @@ class Humanoid(Unit):
         self.variation = variation
         self.hairstyle = hairstyle
 
-        body_path = image if image is not None else f"humanoids/{gender}_{variation}.png"
+        body_path = image if image is not None else f"humanoids/humanoid.png"
         hairstyle_path = f"humanoids/hairstyles/{hairstyle}.png"
 
         super().__init__(
             tile_x, tile_y, key, tag,
             image_file=body_path,
-            width=self.SPRITE_SIZE,
-            height=self.SPRITE_SIZE,
+            width = CONSTANTS.TILE_HEIGHT,
+            height = CONSTANTS.TILE_WIDTH,
         )
 
         body_y_offset = -int(CONSTANTS.TILE_HEIGHT / 2)
         sprite = self.my_images[1]
         sprite.y_offset = body_y_offset
+        # These should prob get moved into unit as soon as all unit sprites have whitespace around them removed
         self.my_images[2].y_offset += body_y_offset
         self.my_images[3].y_offset += body_y_offset
         self.my_images[4].y_offset += body_y_offset
